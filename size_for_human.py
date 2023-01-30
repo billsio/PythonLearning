9253a32ff991097d90bb6ca918ef88149f49b465
wilsonwheaton <123837454+wilsonwheaton@users.noreply.github.com>
2023-01-30 09:55:40 -0600

    Update size_for_human.py
    Now wilson is committing his first change on bill's code

--- size_for_human.py
+++ size_for_human.py
@@ -1,6 +1,6 @@
 ''' writing a new python file '''
 print('\n\nFirst practice python program\n')
-
+print('\n***Wilson is making changes here\n')
 # define dictionary suffixes, keys are 1000 and 1024
 SUFFIXES = {1000: ['KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
 1024: ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB']}
